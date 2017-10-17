593c9a0d902e47e8106188ccf88659261748e917
farrukh90 <farrukhsadykov@gmail.com>
2017-10-17 10:42:30 -0500

    Update rotate.sh

--- rotate.sh
+++ rotate.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-#This command below finds 10 MB files. 
+#This line moves existing files to /mnt folder
 find /tmp  -type f -name error.log.*.gz   -exec mv {} /mnt \; 
+
+#This command below finds 10 MB files. 
 find  /tmp -type f  -size +10M -name error.log  -exec mv {} error.log.$(date +%d.%m.%y).$RANDOM \;
+
+#This  line zipps error.log
 find  /tmp  -type f -mtime -1 -size +10M -name  error.log.* -exec gzip -v9 {} \;
 
