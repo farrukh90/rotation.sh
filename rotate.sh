ac13798b09b5cdddf3c61ac8618252d646e00c8c
farrukh90 <farrukhsadykov@gmail.com>
2017-10-17 10:33:41 -0500

    Update rotate.sh

--- rotate.sh
+++ rotate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #This command below finds 10 MB files. 
+find /tmp  -type f -name error.log.*   -exec mv {} /mnt \; 
 find  /tmp -type f  -size +10M -name error.log  -exec mv {} error.log.$(date +%d.%m.%y) \;
 find  /tmp  -type f -mtime -1 -size +10M -name  error.log.* -exec gzip -v9 {} \;
 
